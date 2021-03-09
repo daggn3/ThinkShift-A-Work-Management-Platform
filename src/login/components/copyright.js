ab358909377c07231b6e3db3b984241776659c67
Niall Dagg <daggbros@gmail.com>
2021-03-09 11:44:30 +0000

    yo

--- src/login/components/copyright.js
+++ src/login/components/copyright.js
@@ -7,7 +7,7 @@ function Copyright() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
         {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
+        <Link color="inherit" href="http://ec2-34-245-24-4.eu-west-1.compute.amazonaws.com:3000">
           ThinkShift
         </Link>{' '}
         {new Date().getFullYear()}
