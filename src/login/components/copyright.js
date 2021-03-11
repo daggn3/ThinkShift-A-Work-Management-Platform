461f15ff8f2e23eaf104d080793e3b9c89f6b24e
Niall Dagg <daggbros@gmail.com>
2021-03-11 11:13:58 +0000

    m

--- src/login/components/copyright.js
+++ src/login/components/copyright.js
@@ -6,12 +6,10 @@ import {Typography, Link} from '@material-ui/core'
 function Copyright() {
     return (
       <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="http://ec2-34-245-24-4.eu-west-1.compute.amazonaws.com:3000">
-          ThinkShift
+        {'Find Out More at: '}
+        <Link color="inherit" href="https://gitlab.computing.dcu.ie/daggn3/2021-ca326-ndagg-thinkshift">
+          ThinkShift GitLab Repo.
         </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
       </Typography>
     );
   }
